840f6e8cf326b6762645ad523ea60b9d8e8b80eb
vmonge79 <vivi_monge@hotmail.com>
2024-11-28 15:14:03 -0600

    chamber2811

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -203,3 +203,23 @@ async function fetchWeather() {
 
 fetchWeather();
 
+/*MODAL*/
+
+function openModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = "block";
+}
+
+function closeModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    var modals = document.getElementsByClassName("modal");
+    for (var i = 0; i < modals.length; i++) {
+        if (event.target == modals[i]) {
+            modals[i].style.display = "none";
+        }
+    }
+}
