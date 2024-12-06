06fe088f0d61a69b0eb18bc5fb416f30f50cd578
vmonge79 <vivi_monge@hotmail.com>
2024-12-06 15:54:14 -0600

    web0612

--- website/scripts/well.js
+++ website/scripts/well.js
@@ -3,7 +3,7 @@ const hambutton = document.querySelector('#menu');
 
 hambutton.addEventListener('click', () => {
     mainnav.classList.toggle('show');
-    hambutton.classList.toggle('show');
+    hambutton.classList.toggle('active'); 
 });
 
 
