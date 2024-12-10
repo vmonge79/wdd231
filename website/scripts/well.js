ae97530e6a4befea4230e2e57490a1c7edc20109
vmonge79 <vivi_monge@hotmail.com>
2024-12-10 12:18:25 -0600

    web1012

--- website/scripts/well.js
+++ website/scripts/well.js
@@ -7,3 +7,26 @@ hambutton.addEventListener('click', () => {
 });
 
 
+/*CARDS*/
+
+fetch('data/wellness.json')
+    .then(response => response.json())
+    .then(products => {
+        createProductCards(products);
+    })
+    .catch(error => console.error('Error loading JSON:', error));
+
+function createProductCards(products) {
+    const cardsContainer = document.querySelector('.cards');
+    products.forEach(product => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = `
+            <img src="${product.icon}" alt="${product.product}" />
+            <h3>${product.product}</h3>
+            <p>${product.description.replace(/\n/g, '<br>')}</p>
+            <a href="${product['more-info']}" target="_blank">More Info</a>
+        `;
+        cardsContainer.appendChild(card);
+    });
+}
