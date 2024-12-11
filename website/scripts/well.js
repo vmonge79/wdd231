3de7963a91a6521d0adf5ad05c33a5dfb772d475
vmonge79 <vivi_monge@hotmail.com>
2024-12-10 20:02:16 -0600

    web1012_2

--- website/scripts/well.js
+++ website/scripts/well.js
@@ -30,3 +30,52 @@ function createProductCards(products) {
         cardsContainer.appendChild(card);
     });
 }
+
+/*Benefits*/
+
+fetch('data/key-benefits.json')
+    .then(response => response.json())
+    .then(products => {
+        createBenefitCards(products);
+    })
+    .catch(error => console.error('Error loading JSON:', error));
+
+function createBenefitCards(products) {
+    const cardsContainer = document.querySelector('.health');
+    products.forEach(product => {
+        const card = document.createElement('div');
+        card.classList.add('card2');
+        card.innerHTML = `
+            <div class="card2-icon">
+                <img src="${product.icon}" alt="${product.product}" />
+            </div>
+            <h3 class="card2-product">${product.product.replace(/\n/g, '<br>')}</h3>
+            <p class="card2-description">${product.description}</p>
+        `;
+        cardsContainer.appendChild(card);
+    });
+}
+
+
+fetch('data/income.json')
+    .then(response => response.json())
+    .then(incomes => {
+        createIncomeCards(incomes);
+    })
+    .catch(error => console.error('Error loading JSON:', error));
+
+function createIncomeCards(incomes) {
+    const cardsContainer = document.querySelector('.income');
+    incomes.forEach(income => {
+        const card = document.createElement('div');
+        card.classList.add('card2');
+        card.innerHTML = `
+            <div class="card2-icon">
+                <img src="${income.icon}" alt="${income.title}" />
+            </div>
+            <h3 class="card2-product">${income.title.replace(/\n/g, '<br>')}</h3>
+            <p class="card2-description">${income.description}</p>
+        `;
+        cardsContainer.appendChild(card);
+    });
+}
